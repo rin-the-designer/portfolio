bf9fc982a88ea1e7039ce5da09fad9548d2cab7a
rin-the-designer <me@rinchong.kim>
2025-01-18 17:11:50 -0500

    [...slug] prerender error fix

--- src/routes/archive/[...slug]/+page.svelte
+++ src/routes/archive/[...slug]/+page.svelte
@@ -3,6 +3,8 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+	export const prerender = true;
+	export const trailingSlash = 'never';
 
 	$: ({ item } = data);
 </script>
